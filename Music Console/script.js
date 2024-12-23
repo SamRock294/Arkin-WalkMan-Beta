14b88bacaeb2aea80f4d4effc177e6b224270b94
R Sakaeth Ram <sakaethrambusiness@gmail.com>
2024-12-23 21:13:42 +0530

    script.js

--- Music Console/script.js
+++ Music Console/script.js
@@ -1,24 +1,24 @@
 document.getElementById("demonSlayer-list").onclick = function() {
-    window.location.href = "file:///C:/Users/sakae/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Demon%20Slayer%20Music/index.html";
+    window.location.href = "file:///C:/Users/Username/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Demon%20Slayer%20Music/index.html";
   };
 
   document.getElementById("oshiNK-list").onclick = function() {
-    window.location.href = "file:///C:/Users/sakae/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Oshi%20No%20Ko%20Music/index.html";
+    window.location.href = "file:///C:/Users/Username/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Oshi%20No%20Ko%20Music/index.html";
   };
 
   document.getElementById("bleach-list").onclick = function() {
-    window.location.href = "file:///C:/Users/sakae/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Bleach%20Music/index.html";
+    window.location.href = "file:///C:/Users/Username/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Bleach%20Music/index.html";
   };
 
 
   document.getElementById("demonSlayer-tier").onclick = function() {
-    window.location.href = "file:///C:/Users/sakae/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Demon%20Slayer%20Music/index.html";
+    window.location.href = "file:///C:/Users/Username/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Demon%20Slayer%20Music/index.html";
   };
 
   document.getElementById("oshiNK-tier").onclick = function() {
-    window.location.href = "file:///C:/Users/sakae/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Oshi%20No%20Ko%20Music/index.html";
+    window.location.href = "file:///C:/Users/Username/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Oshi%20No%20Ko%20Music/index.html";
   };
 
   document.getElementById("bleach-tier").onclick = function() {
-    window.location.href = "file:///C:/Users/sakae/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Bleach%20Music/index.html";
+    window.location.href = "file:///C:/Users/Username/OneDrive/Documents/Projects/Arkin%20WalkMan/Music%20Player/Bleach%20Music/index.html";
   };
